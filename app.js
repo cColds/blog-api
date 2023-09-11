3df259d3eac5533b2a96b88c4ea5b399ed60929e
cColds <103373668+cColds@users.noreply.github.com>
2023-09-11 12:45:08 -0500

    Setup express app

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const apiRouter = require("./routes/api");
+
+const app = express();
+
+function connectMongoDB() {
+    try {
+        mongoose.connect(process.env.MONGODB_URI);
+    } catch (error) {
+        console.error("Failed to connect to MongoDB");
+    }
+}
+
+connectMongoDB();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+app.use("/api", apiRouter);
+
+module.exports = app;
