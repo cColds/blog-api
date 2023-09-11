27e38c399c195f5c1a706cef103e4fe99a5cf2b5
cColds <103373668+cColds@users.noreply.github.com>
2023-09-11 13:54:27 -0500

    Add post model

---
+++ models/Post.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+const postSchema = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    body: { type: String, required: true },
+    date: { type: Date, default: Date.now() },
+    published: { type: Boolean, required: true },
+});
+
+const Post = model("Post", postSchema);
+
+module.exports = Post;
