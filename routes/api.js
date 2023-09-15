1a7fc1c1e02a1790d091a8f9558263953f25dd87
cColds <103373668+cColds@users.noreply.github.com>
2023-09-15 14:36:57 -0500

    Add update blog

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const multer = require("multer");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -112,5 +113,65 @@ router.get("/blogs/:blogId", async (req, res) => {
 
     res.json(blog);
 });
+
+router.post(
+    "/blogs/:blogId",
+    upload.single("image"),
+    body("title").trim().notEmpty().withMessage("Title cannot be empty"),
+    body("author").trim().notEmpty().withMessage("Author cannot be empty"),
+    body("body").trim().notEmpty().withMessage("Body cannot be empty"),
+    check("image").custom((value, { req }) => {
+        const validImageExts = [
+            "image/webp",
+            "image/png",
+            "image/jpeg",
+            "image/avif",
+        ];
+
+        if (req.file && !validImageExts.includes(req.file.mimetype)) {
+            throw new Error(
+                "File extension must be .webp, .png, .jpg/jpeg, or .avif"
+            );
+        }
+
+        return true;
+    }),
+    body("published")
+        .isIn(["true", "false"])
+        .withMessage("Published must be true/false"),
+    verifyToken,
+    async (req, res) => {
+        try {
+            const img = req.file
+                ? {
+                      data: Buffer.from(req.file.buffer).toString("base64"),
+                      contentType: req.file.mimetype,
+                  }
+                : undefined;
+
+            const updatedBlog = {
+                _id: new mongoose.Types.ObjectId(req.params.blogId),
+                title: req.body.title,
+                author: req.body.author,
+                img,
+            };
+
+            if (req.file) {
+                updatedBlog.img = img;
+            } else if (!req.img) {
+                updatedBlog.$unset = { img: 1 }; // This will remove the 'file' field from the document
+            }
+
+            const blog = await Post.findByIdAndUpdate(
+                req.params.blogId,
+                updatedBlog
+            );
+            res.json(blog);
+        } catch (e) {
+            console.error(e);
+            res.sendStatus(404);
+        }
+    }
+);
 
 module.exports = router;
