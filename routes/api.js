d42809f4a27eb1d0d13a1c21fc39af3ccc806385
cColds <103373668+cColds@users.noreply.github.com>
2023-09-14 20:12:12 -0500

    Add blogs (POST) route

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const multer = require("multer");
+const { body, check, validationResult } = require("express-validator");
 const Post = require("../models/Post");
+
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage,
+});
 
 const router = express.Router();
 
@@ -8,5 +15,57 @@ router.get("/blogs", async (req, res) => {
 
     res.json(blogs);
 });
+
+router.post(
+    "/blogs",
+    upload.single("image"),
+    body("title").trim().notEmpty().withMessage("Title cannot be empty"),
+    body("author").trim().notEmpty().withMessage("Author cannot be empty"),
+    body("body").trim().notEmpty().withMessage("Body cannot be empty"),
+    check("image").custom((value, { req }) => {
+        const validImageExts = [
+            "image/webp",
+            "image/png",
+            "image/jpeg",
+            "image/avif",
+        ];
+
+        if (req.file && !validImageExts.includes(req.file.mimetype)) {
+            throw new Error(
+                "File extension must be .webp, .png, .jpg/jpeg, or .avif"
+            );
+        }
+
+        return true;
+    }),
+    body("published")
+        .isIn(["true", "false"])
+        .withMessage("Published must be true/false"),
+    async (req, res) => {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            res.json(result.errors);
+            return;
+        }
+
+        const img = req.file
+            ? {
+                  data: Buffer.from(req.file.buffer).toString("base64"),
+                  contentType: req.file.mimetype,
+              }
+            : undefined;
+        const blog = new Post({
+            title: req.body.title,
+            author: req.body.author,
+            body: req.body.body,
+            published: JSON.parse(req.body.published),
+            img,
+        });
+
+        await blog.save();
+
+        res.json(blog);
+    }
+);
 
 module.exports = router;
