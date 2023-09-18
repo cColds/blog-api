adf1a29553ec6d93d9a0b8fdb843d0240cf42464
cColds <103373668+cColds@users.noreply.github.com>
2023-09-18 01:20:24 -0500

    Add date virtuals

--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,7 @@
 const { Schema, model } = require("mongoose");
+const format = require("date-fns/format");
+const intervalToDuration = require("date-fns/intervalToDuration");
+const formatDistanceToNow = require("date-fns/formatDistanceToNow");
 
 const postSchema = new Schema(
     {
@@ -17,6 +20,30 @@ postSchema.virtual("imgUrl").get(function getImgUrl() {
     if (!this.img.data) return "";
     const base64 = this.img.data.toString("base64");
     return `data:${this.img.contentType};base64,${base64}`;
+});
+
+postSchema.virtual("formatDate").get(function formatDate() {
+    const dateDiff = intervalToDuration({
+        start: new Date(this.date),
+        end: new Date(),
+    });
+
+    if (!dateDiff.months) {
+        const dateDistanceInWords = formatDistanceToNow(new Date(this.date));
+        return `${dateDistanceInWords} ago`;
+    }
+
+    const formattedDate = format(new Date(this.date), "MMM d, y");
+    return formattedDate;
+});
+
+postSchema.virtual("formatDateTitle").get(function formatDateTitle() {
+    const formattedDateTitle = format(
+        new Date(this.date),
+        "EEEE, MMMM d, y 'at' hh:mm:ss a"
+    );
+
+    return formattedDateTitle;
 });
 
 const Post = model("Post", postSchema);
