f4465b88829543756f8d5b4b286043c4f5698527
cColds <103373668+cColds@users.noreply.github.com>
2023-09-17 19:57:29 -0500

    add imgUrl virtual to post schema

--- models/Post.js
+++ models/Post.js
@@ -6,11 +6,14 @@ const postSchema = new Schema({
     body: { type: String, required: true },
     date: { type: Date, default: Date.now() },
     published: { type: Boolean, required: true },
-    img: {
-        data: Buffer,
-        contentType: String,
-    },
+    img: { data: Buffer, contentType: String },
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+});
+
+postSchema.virtual("imgUrl").get(function getImgUrl() {
+    if (!this.img.data) return "";
+    const base64 = this.img.data.toString("base64");
+    return `data:${this.img.contentType};base64,${base64}`;
 });
 
 const Post = model("Post", postSchema);
